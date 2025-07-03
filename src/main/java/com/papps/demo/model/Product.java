ca296dc069a8d86035c359824c0941b686240af5
AbhiLtd <abbyind.manu@gmail.com>
2025-07-03 20:29:32 +0530

    resolved the pom conflicts.

--- src/main/java/com/papps/demo/model/Product.java
+++ src/main/java/com/papps/demo/model/Product.java
@@ -1,6 +1,6 @@
 package com.papps.demo.model;
 
-import lombok.AllArgsConstructor;
+/*import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,7 +8,7 @@ import lombok.ToString;
 @Setter
 @Getter
 @ToString //@Data
-@AllArgsConstructor
+@AllArgsConstructor*/
 public class Product {
 
 	private Integer id;
@@ -19,7 +19,6 @@ public class Product {
     	
     }
 	public Product(Integer id, String name, Long price) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.price = price;
